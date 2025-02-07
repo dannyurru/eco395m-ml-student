e0389a032f123e7caa041f2cdc65673e75c2df11
Danny Urrutia <daniel.urrutia@utexas.edu>
2025-02-06 22:40:21 -0600

    question 4

--- homework/1-homework/triangle/oop_triangles.py
+++ homework/1-homework/triangle/oop_triangles.py
@@ -4,18 +4,29 @@ class IsoscelesTriangle(BaseTriangle):
     
     def __init__(self, leg, base):
 
-        self.leg = leg
-        self.base = base
+       self.side_a = leg
+       self.side_b = leg
+       self.side_c = base
+       self.base = base
+       self.height = (leg**2 - (base / 2)**2) ** (1/2)
+       BaseTriangle.__init__(self, self.side_a, self.side_b, self.side_c, self.base, self.height)
 
     def _get_sides(self):
-
-        return (self.leg, self.leg, self.base)
+          return (self.side_a, self.side_b, self.side_c)
     
     # leave as an exercise
     def _get_base_height(self):
-        pass
+        return (self.base, self.height)
     
 class RightTriangle(BaseTriangle):
+    def __init__(self, leg_a, leg_b):
+        self.side_a = leg_a
+        self.side_b = leg_b
+        self.side_c = (leg_a**2 + leg_b**2) ** 0.5
+        self.base = leg_a
+        self.height = leg_b
+        BaseTriangle.__init__(self, self.side_a, self.side_b, self.side_c, self.base, self.height)
+
     pass
     
 if __name__ == "__main__":
