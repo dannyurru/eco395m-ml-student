5de6cfc0ff6b4015c708c718f214f10f02f84a6e
Danny Urrutia <daniel.urrutia@utexas.edu>
2025-02-06 23:13:39 -0600

    question 5

--- homework/1-homework/diy_regressor/knn.py
+++ homework/1-homework/diy_regressor/knn.py
@@ -1,16 +1,46 @@
 import numpy as np
 
-# students to implement
 class KNeighborsRegressor():
-
     def __init__(self, n_neighbors):
-        pass
-    
+        self.n_neighbors = n_neighbors
+        self.X_train = None
+        self.y_train = None
+
     def fit(self, X, y):
-        pass        
+        self.X_train = X
+        self.y_train = y
 
     def predict(self, X):
-        pass
+        predictions = []
+        
+        for i, test_point in enumerate(X):
+            distances = []
+
+            for x, train_point in enumerate(self.X_train):
+                distance = abs(train_point[0] - test_point[0])
+                distances.append((distance, x))
+
+            for i in range(len(distances) - 1):  
+                for j in range(i + 1, len(distances)):  
+                    if distances[i][0] > distances[j][0]:
+                        distances[i], distances[j] = distances[j], distances[i]
+
+            neighbor_indices = [distances[x][1] for x in range(min(self.n_neighbors, len(distances)))]
+
+            neighbor_values = []
+            for index in neighbor_indices:
+                neighbor_values.append(self.y_train[index])
+
+            total = 0
+            count = 0
+            for value in neighbor_values:
+                total += value
+                count += 1
+            mean_prediction = total / count
+
+            predictions.append(mean_prediction)
+
+        return np.array(predictions)
     
 if __name__ == "__main__":
 
